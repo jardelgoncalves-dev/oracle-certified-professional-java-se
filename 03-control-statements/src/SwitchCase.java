ee08fe699cedef75dca3db5630b3fda2ace4d52f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-17 23:52:01 -0300

    add switch/case sample

---
+++ 03-control-statements/src/SwitchCase.java
@@ -0,0 +1,23 @@
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		char letter = 'A';
+		
+		switch(letter) {
+			case 'C':
+				System.out.println("letter is C");
+				break;
+			case 'D':
+				System.out.println("letter is D");
+				break;
+			case 'A':
+				System.out.println("letter is A");
+				break;
+			default:
+				System.out.println("letter is not found");
+		}
+
+	}
+
+}
